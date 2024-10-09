e9bf0b8654a86c69522a9559b7a39deb5d268fe3
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-10-09 12:43:23 -0700

    Reformat

--- cursor/tools.scm
+++ cursor/tools.scm
@@ -1,50 +1,50 @@
 (library (cursor tools)
-  (export assert
-          enum
-          zip-with
-          scan-right
-          reduce-right)
-  (import (rnrs))
+         (export assert
+                 enum
+                 zip-with
+                 scan-right
+                 reduce-right)
+         (import (rnrs))
 
-  (define-syntax assert
-    (syntax-rules ()
-      [(_ compare x y)
-       (let ([computed-x x]
-             [computed-y y])
-         (unless (compare computed-x computed-y)
-           (begin (display "Test failed:")
-                  (newline)
-                  (display "lhs: ") (write (quote x)) (display " -> ") (write computed-x) (display ", ")
-                  (display "rhs: ") (write (quote y)) (display " -> ") (write computed-y)
-                  (newline))))]))
-  
-  (define-syntax enum
-    (syntax-rules ()
-      [(_ x y ...)
-       (begin (define x (quote x))
-              (define y (quote y)) ...)]))
-  
-  (define zip-with
-    (lambda (fn xs ys)
-      (if (or (null? xs) (null? ys))
-          '()
-          (cons (fn (car xs) (car ys))
-                (zip-with fn (cdr xs) (cdr ys))))))
+         (define-syntax assert
+           (syntax-rules ()
+             [(_ compare x y)
+              (let ([computed-x x]
+                    [computed-y y])
+                (unless (compare computed-x computed-y)
+                  (begin (display "Test failed:")
+                         (newline)
+                         (display "lhs: ") (write (quote x)) (display " -> ") (write computed-x) (display ", ")
+                         (display "rhs: ") (write (quote y)) (display " -> ") (write computed-y)
+                         (newline))))]))
 
-  (define scan-right
-    (lambda (fn base xs)
-      (if (null? xs)
-          xs
-          (let ([x (fn base (car xs))])
-            (cons x (scan-right fn x (cdr xs)))))))
+         (define-syntax enum
+           (syntax-rules ()
+             [(_ x y ...)
+              (begin (define x (quote x))
+                     (define y (quote y)) ...)]))
 
-  (define reduce-right
-    (lambda (fn xs)
-      (fold-right (lambda (x accum)
-                    (if (null? accum)
-                        x
-                        (fn x accum)))
-                  '()
-                  xs)))
-  
-  )
+         (define zip-with
+           (lambda (fn xs ys)
+             (if (or (null? xs) (null? ys))
+                 '()
+                 (cons (fn (car xs) (car ys))
+                       (zip-with fn (cdr xs) (cdr ys))))))
+
+         (define scan-right
+           (lambda (fn base xs)
+             (if (null? xs)
+                 xs
+                 (let ([x (fn base (car xs))])
+                   (cons x (scan-right fn x (cdr xs)))))))
+
+         (define reduce-right
+           (lambda (fn xs)
+             (fold-right (lambda (x accum)
+                           (if (null? accum)
+                               x
+                               (fn x accum)))
+                         '()
+                         xs)))
+
+         )
