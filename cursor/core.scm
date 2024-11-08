2fd662467fce6da2ff8ce1496b45a58e63a9b87b
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-11-07 20:40:48 -0800

    Move constants and data to separate file

--- cursor/core.scm
+++ cursor/core.scm
@@ -21,36 +21,6 @@
                  (cursor tools)
                  (cursor collections charset))
 
-         ;; === Constants ===
-
-         (enum ERROR
-               EMPTY
-               ANY
-               FAIL
-               FAIL-TWICE
-               CHARACTER
-               SEQUENCE
-               CHOICE
-               COMMIT
-               PARTIAL-COMMIT
-               BACK-COMMIT
-               GRAMMAR
-               RULE
-               CALL
-               OPEN-CALL
-               RETURN
-               JUMP
-               CAPTURE-START
-               CAPTURE-STOP
-               REPEAT
-               IS
-               IS-NOT
-               ONE-OF
-               NONE-OF
-               MATCH)
-
-         (define MAX-RULES 1000)
-
          ;; === Error Messages ===
 
          (define ERROR-TYPE-CHARACTER "not a character")
@@ -62,25 +32,6 @@
          (define ERROR-UNDEFINED-RULE "undefined rule in grammar")
          (define ERROR-NULLABLE       "expression within may cause infinite loop")
          (define ERROR-LEFT-RECURSION "rule may be left recursive")
-
-         ;; === Data ===
-
-         ;; Instruction Set: (list code code ...)
-         ;;   where code = (encode type op-x op-y)
-
-         ;; record-type: code
-         ;;
-         ;; An instruction containing a type identifier followed by two operands.
-         (define-record-type (code encode code?)
-           (fields type op-x op-y)
-           (nongenerative)
-           (sealed #t)
-           (protocol
-            (lambda (new)
-              (case-lambda
-                [(type)           (new type '() '())]
-                [(type op-x)      (new type op-x '())]
-                [(type op-x op-y) (new type op-x op-y)]))))
 
          ;; === Helper Functions ===
 
