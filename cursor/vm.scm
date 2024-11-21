6a71c62ca704c09e3f93dee0895e15c64ee9cc80
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2024-11-20 21:06:35 -0800

    Expand "fail-state"

--- cursor/vm.scm
+++ cursor/vm.scm
@@ -5,16 +5,11 @@
                  (cursor tools))
 
          (define-record-type entry
-           (fields ip
-                   (mutable sp)
-                   stack
-                   captures)
+           (fields ip (mutable sp) captures)
            (sealed #t))
 
          (define-record-type capture
-           (fields type
-                   function
-                   offset)
+           (fields type function offset)
            (sealed #t)
            (protocol
             (lambda (new)
@@ -38,7 +33,6 @@
                                                  (+ sp 1)
                                                  stack
                                                  captures)
-                                          ;; === todo ===
                                           (fail-state ip sp stack captures))]
                                      [(eq? type ANY)
                                       (if (< sp size)
@@ -68,7 +62,18 @@
                                                       sp
                                                       stack
                                                       captures)))]
-                                     [(eq? type CALL)
+                                     [(eq? type FAIL)
+                                      (fail-state ip
+                                                  sp
+                                                  stack
+                                                  captures)]
+                                     [(eq? type FAIL-TWICE)
+                                      (fail-state ip
+                                                  sp
+                                                  (cdr stack)
+                                                  captures)]
+                                     [(or (eq? type CALL)
+                                          (eq? type GRAMMAR))
                                       (state op-y
                                              sp
                                              (cons (+ ip 1) stack)
@@ -85,7 +90,6 @@
                                                  (+ sp 1)
                                                  stack
                                                  captures)
-                                          ;; === todo ===
                                           (fail-state ip sp stack captures))]
                                      [(eq? type NONE-OF)
                                       (if (and (< sp size)
@@ -94,7 +98,6 @@
                                                  (+ sp 1)
                                                  stack
                                                  captures)
-                                          ;; === todo ===
                                           (fail-state ip sp stack captures))]
                                      [(eq? type CAPTURE-START)
                                       (state (+ ip 1)
@@ -105,10 +108,23 @@
                                       (state (+ ip 1)
                                              sp
                                              stack
-                                             (cons (make-capture CAPTURE-STOP sp) captures))]))))]
+                                             (cons (make-capture CAPTURE-STOP sp) captures))]
+                                     [(eq? type MATCH)
+                                      (cons sp captures)]))))]
                         [fail-state
                          (lambda (ip sp stack captures)
-                           'todo)])
+                           (if (null? stack)
+                               0
+                               (let ([entry (car stack)])
+                                 (if (number? entry)
+                                     (fail-state ip
+                                                 sp
+                                                 (cdr stack)
+                                                 captures)
+                                     (state (entry-ip entry)
+                                            (entry-sp entry)
+                                            (cdr stack)
+                                            (entry-captures entry))))))])
                  ;; === start virtual machine ===
                  (state 0 0 '() '())))))
 
