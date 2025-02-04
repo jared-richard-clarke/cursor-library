685e2ebfd6bbd512584e130a09973390cbd24449
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2025-02-04 12:04:05 -0800

    Rebuild virtual machine

--- cursor/vm.scm
+++ cursor/vm.scm
@@ -2,7 +2,10 @@
          (export run-vm)
          (import (rnrs)
                  (cursor data)
-                 (cursor tools))
+                 (cursor tools)
+                 (cursor collections charset))
+
+         (define ERROR-VM "unrecognized instruction")
 
          (define-record-type entry
            (fields ip (mutable sp) (mutable captures))
@@ -13,120 +16,120 @@
            (sealed #t))
 
          (define run-vm
-           (lambda (subject instructions)
-             (let ([size (vector-length subject)])
+           (lambda (text program)
+             (let ([size (length text)])
                (letrec ([state
                          (lambda (ip sp stack captures)
-                           (let ([instruction (vector-ref instructions ip)])
-                             (let ([type (code-type instruction)]
-                                   [op-x (code-op-x instruction)]
-                                   [op-y (code-op-y instruction)])
-                               (cond [(eq? type CHARACTER)
-                                      (if (and (< sp size)
-                                               (char=? op-x (vector-ref subject sp)))
-                                          (state (+ ip 1)
-                                                 (+ sp 1)
-                                                 stack
-                                                 captures)
-                                          (fail-state ip sp stack captures))]
-                                     [(eq? type ANY)
-                                      (if (< sp size)
-                                          (state (+ ip 1)
-                                                 (+ sp 1)
-                                                 stack
-                                                 captures))]
-                                     [(eq? type JUMP)
-                                      (state (+ ip op-x)
+                           (let ([code (vector-ref program ip)])
+                             (cond [(char? code)
+                                    (if (and (< sp size)
+                                             (char=? code (vector-ref text sp)))
+                                        (state (+ ip 1)
+                                               (+ sp 1)
+                                               stack
+                                               captures)
+                                        (state-fail ip sp stack captures))]
+                                   [(eq? code ANY)
+                                    (if (< sp size)
+                                        (state (+ ip 1)
+                                               (+ sp 1)
+                                               stack
+                                               captures)
+                                        (fail-state ip sp captures))]
+                                   [(eq? code JUMP)
+                                    (let ([offset (vector-ref program (+ ip 1))])
+                                      (state (+ ip offset)
                                              sp
                                              stack
-                                             captures)]
-                                     [(eq? type CHOICE)
-                                      (state (+ ip 1)
+                                             captures))]
+                                   [(eq? code CHOICE)
+                                    (let ([offset (vector-ref program (+ ip 1))])
+                                      (state (+ ip 2)
                                              sp
-                                             (cons (make-entry (+ ip op-x) sp captures) stack)
-                                             captures)]
-                                     [(eq? type COMMIT)
-                                      (state (+ ip op-x)
+                                             (cons (make-entry (+ ip offset) sp captures) stack)
+                                             captures))]
+                                   [(eq? code COMMIT)
+                                    (let ([offset (vector-ref program (+ ip 1))])
+                                      (state (+ ip offset)
                                              sp
                                              (cdr stack)
-                                             captures)]
-                                     [(eq? type PARTIAL-COMMIT)
-                                      (let ([entry (car stack)])
-                                        (begin (set-entry-sp! entry sp)
-                                               (set-entry-captures! entry captures)
-                                               (state (+ ip op-x)
-                                                      sp
-                                                      stack
-                                                      captures)))]
-                                     [(eq? type BACK-COMMIT)
-                                      (let ([entry (car stack)])
-                                        (state (+ ip op-x)
-                                               (entry-sp entry)
-                                               (cdr stack)
-                                               (entry-captures entry)))]
-                                     [(eq? type FAIL)
-                                      (fail-state ip
-                                                  sp
-                                                  stack
-                                                  captures)]
-                                     [(eq? type FAIL-TWICE)
-                                      (fail-state ip
-                                                  sp
-                                                  (cdr stack)
-                                                  captures)]
-                                     [(or (eq? type CALL)
-                                          (eq? type GRAMMAR))
-                                      (state (+ op-y 1)
+                                             captures))]
+                                   [(eq? code PARTIAL-COMMIT)
+                                    (let ([offset (vector-ref program (+ ip 1))]
+                                          [entry  (car stack)])
+                                      (set-entry-sp! entry sp)
+                                      (set-entry-captures! entry captures)
+                                      (state (+ ip offset)
                                              sp
-                                             (cons (+ ip 1) stack)
-                                             captures)]
-                                     [(eq? type RETURN)
-                                      (state (car stack)
-                                             sp
+                                             stack
+                                             captures))]
+                                   [(eq? code BACK-COMMIT)
+                                    (let ([offset (vector-ref program (+ ip 1))]
+                                          [entry  (car stack)])
+                                      (state (+ ip offset)
+                                             (entry-sp entry)
                                              (cdr stack)
-                                             captures)]
-                                     [(eq? type ONE-OF)
+                                             (entry-captures entry)))]
+                                   [(eq? code FAIL)
+                                    (fail-state ip sp stack captures)]
+                                   [(eq? code FAIL-TWICE)
+                                    (fail-state ip sp (cdr stack) captures)]
+                                   [(eq? code CALL)
+                                    (let ([offset (vector-ref program (+ ip 1))])
+                                      (state (+ ip offset)
+                                             sp
+                                             (cons (+ ip 2) stack)
+                                             captures))]
+                                   [(eq? code RETURN)
+                                    (state (car stack)
+                                           sp
+                                           (cdr stack)
+                                           captures)]
+                                   [(eq? code ONE-OF)
+                                    (let ([set (vector-ref program (+ ip 1))])
                                       (if (and (< sp size)
-                                               (hashtable-contains? op-x (vector-ref subject sp)))
-                                          (state (+ ip 1)
+                                               (charset-has? set (vector-ref text sp)))
+                                          (state (+ ip 2)
                                                  (+ sp 1)
                                                  stack
                                                  captures)
-                                          (fail-state ip sp stack captures))]
-                                     [(eq? type NONE-OF)
+                                          (fail-state ip sp stack captures)))]
+                                   [(eq? code NONE-OF)
+                                    (let ([set (vector-ref program (+ ip 1))])
                                       (if (and (< sp size)
-                                               (not (hashtable-contains? op-x (vector-ref subject sp))))
-                                          (state (+ ip 1)
+                                               (not (charset-has? set (vector-ref text sp))))
+                                          (state (+ ip 2)
                                                  (+ sp 1)
                                                  stack
                                                  captures)
-                                          (fail-state ip sp stack captures))]
-                                     [(eq? type CAPTURE-START)
-                                      (state (+ ip 1)
+                                          (fail-state ip sp stack captures)))]
+                                   [(eq? code CAPTURE-START)
+                                    (let ([operation (vector-ref program (+ ip 1))])
+                                      (state (+ ip 2)
                                              sp
                                              stack
-                                             (cons (make-capture CAPTURE-START op-y sp) captures))]
-                                     [(eq? type CAPTURE-STOP)
-                                      (state (+ ip 1)
-                                             sp
-                                             stack
-                                             (cons (make-capture CAPTURE-STOP op-y sp) captures))]
-                                     [(eq? type MATCH)
-                                      (cons sp captures)]))))]
+                                             (cons (make-capture CAPTURE-START operation sp) captures)))]
+                                   [(eq? code CAPTURE-STOP)
+                                    (state (+ ip 1)
+                                           sp
+                                           stack
+                                           (cons CAPTURE-STOP captures))]
+                                   [(eq? code MATCH)
+                                    (cons sp captures)]
+                                   [else
+                                    (raise (make-peg-error "virtual machine" code ERROR-VM))])))]
                         [fail-state
                          (lambda (ip sp stack captures)
-                           (if (null? stack)
-                               0
-                               (let ([entry (car stack)])
-                                 (if (number? entry)
-                                     (fail-state ip
-                                                 sp
-                                                 (cdr stack)
-                                                 captures)
-                                     (state (entry-ip entry)
-                                            (entry-sp entry)
-                                            (cdr stack)
-                                            (entry-captures entry))))))])
+                           (cond [(null? stack)
+                                  FAIL]
+                                 [else
+                                  (let ([entry (car stack)])
+                                    (if (number? entry)
+                                        (fail-state ip sp (cdr stack) captures)
+                                        (state (entry-ip entry)
+                                               (entry-sp entry)
+                                               (cdr stack)
+                                               (entry-captures entry))))]))])
                  ;; === start virtual machine ===
                  (state 0 0 '() '())))))
 
