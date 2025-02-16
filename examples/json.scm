78ad286f7ecb9f00fb74dfede59764aa3e76c8e4
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2025-02-15 21:40:13 -0800

    Update json.scm

--- examples/json.scm
+++ examples/json.scm
@@ -5,7 +5,7 @@
 
          (define separate-by
            (lambda (x sep)
-             (sequence x (repeat (sequence sep x)))))
+             (and-then x (repeat (and-then sep x)))))
          
          (define whitespace (repeat (one-of " \n\r\t")))
 
@@ -13,63 +13,54 @@
 
          (define digits (repeat+1 digit))
 
-         (define hex (choice digit (one-of "ABCDEFabcdef")))
-
-         (define escape (choice (one-of "\"/\b\f\n\r\t")
-                                (sequence (char #\u) hex hex hex hex)))
-
          (define json-true  (text "true"))
          (define json-false (text "false"))
          (define json-null  (text "null"))
 
-         (define json-character (sequence (is-not? (choice (char #\")
-                                                           (sequence (char #\\)
-                                                                     escape)))
-                                          any))
+         (define json-character (and-then (is? (none-of "\"/\b\f\n\r\t")) any))
 
          (define json-characters (repeat+1 json-character))
 
-         (define json-string (sequence (char #\")
-                                       json-characters
-                                       (char #\")))
+         (define json-string (and-then (char #\") json-characters (char #\")))
 
-         (define sign        (maybe (choice (char #\+) (char #\-))))
-         (define exponent    (maybe (sequence (choice (char #\e) (char #\E))
+         (define sign        (maybe (or-else (char #\+) (char #\-))))
+         (define exponent    (maybe (and-then (or-else (char #\e)
+                                                       (char #\E))
                                               sign
                                               digits)))
-         (define fractional  (maybe (sequence (char #\.) digits)))
-         (define whole       (choice (char #\0)
-                                     (sequence (one-of "123456789")
-                                               (repeat digit))))
-         (define integer     (sequence sign whole))
+         (define fractional  (maybe (and-then (char #\.) digits)))
+         (define whole       (or-else (char #\0)
+                                      (and-then (one-of "123456789")
+                                                (repeat digit))))
+         (define integer     (and-then sign whole))
 
-         (define json-number (sequence integer fractional exponent))
+         (define json-number (and-then integer fractional exponent))
 
          (define json-grammar
-           (grammar [Element (sequence whitespace (rule Value) whitespace)]
+           (grammar [Element (and-then whitespace (rule Value) whitespace)]
                     
-                    [Value   (choice (rule Object)
-                                     (rule Array)
-                                     (rule String)
-                                     (rule Number)
-                                     (rule True)
-                                     (rule False)
-                                     (rule Null))]
+                    [Value   (or-else (rule Object)
+                                      (rule Array)
+                                      (rule String)
+                                      (rule Number)
+                                      (rule True)
+                                      (rule False)
+                                      (rule Null))]
                     
-                    [Object   (sequence (char #\{)
-                                        (choice (rule Members) whitespace)
+                    [Object   (and-then (char #\{)
+                                        (or-else (rule Members) whitespace)
                                         (char #\}))]
                     
                     [Members  (separate-by (rule Member) (char #\,))]
                     
-                    [Member   (sequence whitespace
+                    [Member   (and-then whitespace
                                         (rule String)
                                         whitespace
                                         (char #\:)
                                         (rule Element))]
                     
-                    [Array    (sequence (char #\[)
-                                        (choice (rule Elements) whitespace)
+                    [Array    (and-then (char #\[)
+                                        (or-else (rule Elements) whitespace)
                                         (char #\]))]
 
                     [Elements (separate-by (rule Element) (char #\,)]
