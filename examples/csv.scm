895119921aea38ac3369db94d8a0165ef852e07d
Jared Clarke <80301412+jared-richard-clarke@users.noreply.github.com>
2025-02-21 19:09:19 -0800

    Defin record type for csv

--- examples/csv.scm
+++ examples/csv.scm
@@ -1,7 +1,33 @@
 (library (examples csv)
-         (export (parse-csv))
+         (export (parse-csv
+                  ;; === record-type: csv ===
+                  csv?          ;; predicate
+                  csv-header    ;; field
+                  csv-rows
+                  ;; === record-type: row ===
+                  row?          ;; predicate
+                  row-columns)) ;; field
          (import (rnrs)
                  (cursor))
+
+         (define-record-type csv
+           (fields header rows))
+         
+         ;; Side Note: "columns" should be called "fields", but name conflicts
+         ;; with the "fields" form of "define-record-type".
+         (define-record-type row
+           (fields columns))
+
+         (define collect-fields
+           (lambda (state)
+             (let loop ([xs state]
+                        [ys '()])
+               (cond [(null? xs)
+                      (make-row (reverse ys))]
+                     [(string? (car xs))
+                      (loop (cdr xs) (cons (car xs) ys))]
+                     [else
+                      (cons (make-row (reverse ys)) xs)]))))
 
          (define fullstop
            (lambda (px)
@@ -17,19 +43,25 @@
                         (list->string x))
                       px)))
 
-         (define characters (capture-text (repeat+1 (none-of "\",\n\r")))
+         (define characters (repeat+1 (none-of "\",\n\r"))
          (define text       (and-then (char #\") characters (char #\")))
 
-         (define csv
+         (define csv-grammar
            (fullstop
-            (grammar [File (and-then (rule Header)
-                                     (repeat+1 (rule Row)))]
+            (grammar [File (transform (lambda (state)
+                                        (make-csv (car state) (cdr state)))
+                                      (and-then (rule Header)
+                                                (repeat+1 (rule Row))))]
                      [Header (rule Row)]
-                     [Row    (and-then (separate-by (rule Field) (char #\,))
-                                       (maybe (char #\return))
-                                       (char #\newline))]
-                     [Field  (or-else characters text)])))
+                     [Row    (transform collect-fields
+                                        (and-then (separate-by (rule Field) (char #\,))
+                                                  (maybe (char #\return))
+                                                  (char #\newline)))]
+                     [Field  (or-else (capture-text characters)
+                                      (and-then (char #\")
+                                                (capture-text characters)
+                                                (char #\")))])))
 
-         (define parse-csv (compile csv))
+         (define parse-csv (compile csv-grammar))
 
 )
