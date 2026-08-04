6e1e2933e7ba9a146699e4342e40ada477761702
Jared Clarke <clajar12@gmail.com>
2026-08-03 20:28:40 -0700

    Add segmenter.scm

---
+++ cursor/unicode/grapheme-break/segmenter.scm
@@ -0,0 +1,29 @@
+(library (cursor unicode grapheme-break segmenter)
+         (export segment)
+         (import (rnrs)
+                 (cursor unicode grapheme-break builders dfa)
+                 (cursor unicode grapheme-break builders code-point-tree))
+
+         ;; === Constants ===
+         
+         (define FAIL-START   (car (dfa-fail DFA)))
+         (define FAIL-STOP    (cdr (dfa-fail DFA)))
+         (define START-STATE  (dfa-start DFA))
+         (define ACCEPT-START (car (dfa-accept DFA)))
+         (define ACCEPT-STOP  (cdr (dfa-accept DFA)))
+         (define TRANSITIONS  (dfa-table DFA))
+
+         (define fail-state?
+           (lambda (x)
+             (< x FAIL-STOP)))
+
+         (define accept-state?
+           (lambda (x)
+             (and (<= x ACCEPT-START)
+                  (<  x ACCEPT-STOP))))
+
+         (define next-state
+           (lambda (state symbol)
+             (vector-ref TRANSITIONS (+ state symbol))))
+
+)
